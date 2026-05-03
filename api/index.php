5489bc438a49f50a671a7546f8bb3ec76afeb77f
nimelucy <65805452+nimelucy@users.noreply.github.com>
2026-05-03 16:04:26 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -529,7 +529,7 @@ function simpanSemua() {
     // KIRIM AJAX KE GOOGLE APPS SCRIPT
     $.ajax({
         // 👇👇👇 GANTI URL DI BAWAH INI DENGAN URL DARI GOOGLE APPS SCRIPT KAMU 👇👇👇
-        url: "https://script.google.com/macros/s/AKfycbzC5jIA12shNniNw7yM4hdrflveQcXnx3rdAiygb0R4byvl8_G1sG6qjlTQP1NI5ws/exec", 
+        url: "https://script.google.com/macros/s/AKfycbwgOdCzQtuYEbzFtnV5T2m-aNdNXQobY0aISMEBFRZtOq38UpfVzDh7OZucS9gHc_I/exec", 
         type: "POST",
         contentType: "text/plain;charset=utf-8", 
         data: JSON.stringify(dataKirim),
